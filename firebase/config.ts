c021d03fe50a29daf192f135248bc81512c478d5
Tomas Cuevas <memo_mister398@hotmail.com>
2022-12-12 13:13:41 -0300

    fix: :bug: fix enviroment on firebase config

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PREJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINFSENDERID,
-  appId: process.env.APPID,
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PREJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINFSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig);
