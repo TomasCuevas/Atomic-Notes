e29a118e1cfc08c2caec7671ed9e8c3ee326638f
Tomas Cuevas <memo_mister398@hotmail.com>
2022-12-12 13:08:27 -0300

    chore: :heavy_plus_sign: add new dependencies and tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#ffdd09",
+        orange: "#fd9808",
+        black: "#222233",
+      },
+    },
+  },
+  plugins: [],
+};
