fcea50172b2ecabff520d3cdaf0d6e16c992ac86
Tomas Cuevas <memo_mister398@hotmail.com>
2022-12-12 13:17:05 -0300

    feat: :sparkles: add layout for register and login page

---
+++ layout/AuthLayout.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+interface Props {
+  children: React.ReactNode;
+  description: string;
+  title: string;
+}
+
+export const AuthLayout: React.FC<Props> = ({
+  children,
+  description,
+  title,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+
+      <main className="flex min-h-screen items-center justify-center bg-[url('/assets/backgroundMobile.svg')] bg-cover bg-no-repeat p-4 md:bg-[url('/assets/backgroundDesktop.svg')]">
+        {children}
+      </main>
+    </>
+  );
+};
