3ffe8988b562c5ea4aa27df339ff6804798a9c6d
Tomas Cuevas <memo_mister398@hotmail.com>
2022-12-22 12:56:35 -0300

    style: :truck: rename MobileHeader to Header and style changes

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,25 +7,29 @@ import { MdOutlineMenu } from "react-icons/md";
 //* context *//
 import { UIContext } from "../../context";
 
-export const MobileHeader: React.FC = () => {
+export const Header: React.FC = () => {
   const { toggleSidebar } = useContext(UIContext);
 
   return (
-    <header className="sticky top-0 left-0 z-40 flex h-14 items-center  bg-orange p-4">
+    <header className="sticky top-0 left-0 z-40 flex h-14 items-center  bg-orange p-4 sm:h-16 md:h-[70px]">
       <NextLink href="/" className="flex items-center gap-4">
-        <img src="/icon.svg" alt="Atomic Notes icon" className="h-10 w-10" />
-        <h1 className="text-xl font-light text-white">
-          <span className="text-2xl font-bold">A</span>
+        <img
+          src="/icon.svg"
+          alt="Atomic Notes icon"
+          className="h-10 w-10 md:h-12 md:w-12"
+        />
+        <h1 className="text-xl font-light text-white md:text-2xl">
+          <span className="text-2xl font-bold md:text-3xl">A</span>
           <span className="font-bold">t</span>
           omic
-          <span className="text-2xl font-bold"> N</span>o
+          <span className="text-2xl font-bold md:text-3xl"> N</span>o
           <span className="font-bold">t</span>
           es
         </h1>
       </NextLink>
       <MdOutlineMenu
         onClick={toggleSidebar}
-        className="ml-auto text-4xl text-black"
+        className="ml-auto text-4xl text-black lg:hidden"
       />
     </header>
   );
