040d6a97d9b512449203bd6c19adc04f5f7a392e
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-15 20:31:14 -0300

    refactor: :truck: rename SiebarItemGreen for SidebarNewNote

--- components/sidebar/SidebarNewNote.tsx
+++ components/sidebar/SidebarNewNote.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import { IconType } from "react-icons";
 
 //* interface *//
@@ -8,20 +7,19 @@ interface Props {
   text: string;
 }
 
-export const SidebarItemGreen: React.FC<Props> = ({
+export const SidebarNewNote: React.FC<Props> = ({
   icon: Icon,
   text,
   whenClick,
 }) => {
-  const router = useRouter();
 
   return (
     <li
       onClick={whenClick}
-      className="group flex cursor-pointer items-center gap-4 rounded-full bg-[#00a82d] px-4 py-2 transition-all duration-300 hover:bg-[#008f26]"
+      className="group flex cursor-pointer items-center gap-4 rounded-full bg-greenLight px-4 py-2 transition-all duration-300 hover:bg-green"
     >
       <div className="flex items-center justify-center rounded-full bg-white p-2">
-        <Icon className="h-4 w-4 text-[#00a82d] group-hover:text-[#008f26]" />
+        <Icon className="h-4 w-4 text-greenLight group-hover:text-green" />
       </div>
       <span className="text-base font-medium text-white">{text}</span>
     </li>
--- components/sidebar/index.ts
+++ components/sidebar/index.ts
@@ -1,6 +1,6 @@
 export * from "./DesktopSidebar";
 export * from "./MobileSidebar";
 export * from "./SidebarItem";
-export * from "./SidebarItemGreen";
+export * from "./SidebarNewNote";
 export * from "./SidebarLogout";
 export * from "./SidebarProfile";
