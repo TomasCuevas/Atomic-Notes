c7a5dacc4906a0191628be9b22f67f2cb0914b6e
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-01 15:03:18 -0300

    feat: :sparkles: add new methods and properties on NotesContext

--- context/NotesContext.tsx
+++ context/NotesContext.tsx
@@ -1,7 +1,14 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 //* services *//
-import { getAllNotesService } from "../services";
+import {
+  createNewNoteService,
+  deleteNoteService,
+  getAllNotesService,
+  getNoteService,
+  updateNoteService,
+} from "../services";
 
 //* context *//
 import { AuthContext } from "./AuthContext";
@@ -12,8 +19,14 @@ import { INote } from "../interfaces/INote";
 //* CONTEXT *//
 //* CONTEXT *//
 interface NotesContextProps {
+  note: INote | undefined;
   notes: INote[];
+  startClearNote(): void;
+  startCreatingNote(title: string, body: string): Promise<string | false>;
+  startDeleteNote(noteId: string): Promise<boolean>;
   startLoadingNotes(): void;
+  startSetNote(noteId: string): void;
+  startUpdateNote(note: INote): Promise<boolean>;
 }
 
 export const NotesContext = createContext({} as NotesContextProps);
@@ -26,7 +39,11 @@ interface NotesProviderProps {
 
 export const NotesProvider: React.FC<NotesProviderProps> = ({ children }) => {
   const { authState, user } = useContext(AuthContext);
+
+  const [note, setNote] = useState<INote>();
   const [notes, setNotes] = useState<INote[]>([]);
+
+  const router = useRouter();
 
   useEffect(() => {
     if (authState === "authenticated") {
@@ -34,19 +51,72 @@ export const NotesProvider: React.FC<NotesProviderProps> = ({ children }) => {
     }
   }, [authState]);
 
+  //* load notes
   const startLoadingNotes = async () => {
     const allNotes = await getAllNotesService(user!.uid);
     if (allNotes) setNotes(allNotes);
+  };
+
+  //* create note
+  const startCreatingNote = async (
+    title: string,
+    body: string
+  ): Promise<string | false> => {
+    const id = await createNewNoteService(user!.uid, title, body);
+
+    if (id) {
+      return id;
+    } else {
+      return false;
+    }
+  };
+
+  //* set note
+  const startSetNote = async (noteId: string) => {
+    const note = await getNoteService(user!.uid, noteId);
+    if (note) {
+      setNote(note);
+    } else {
+      router.replace("/notes/all");
+    }
+  };
+
+  //* clear note
+  const startClearNote = () => setNote(undefined);
+
+  //* update note
+  const startUpdateNote = async (note: INote): Promise<boolean> => {
+    const wasUpdated = await updateNoteService(user!.uid, note);
+
+    if (wasUpdated) {
+      startLoadingNotes();
+      startSetNote(note.id!);
+
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  //* delete note
+  const startDeleteNote = async (noteId: string) => {
+    return await deleteNoteService(user!.uid, noteId);
   };
 
   return (
     <NotesContext.Provider
       value={{
         // getters
+        note,
         notes,
 
         // methods
+        startClearNote,
+        startCreatingNote,
+        startDeleteNote,
         startLoadingNotes,
+        startSetNote,
+        startUpdateNote,
       }}
     >
       {children}
