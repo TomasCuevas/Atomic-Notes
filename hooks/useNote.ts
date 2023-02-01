f6b8e9aef0ec644f49d7a16c9f7ff4799eb31cc1
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-31 23:57:13 -0300

    feat: :sparkles: add useNote hook

---
+++ hooks/useNote.ts
@@ -0,0 +1,40 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
+export const useNote = (bodyValue?: string, titleValue?: string) => {
+  const [body, setBody] = useState(bodyValue || "");
+  const [title, setTitle] = useState(titleValue || "");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isDisabled, setIsDisabled] = useState(title.length < 1);
+
+  //* change title
+  const onChangeTitle = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setTitle(target.value);
+  };
+
+  //* set error
+  const onSetErrorMessage = (message: string) => {
+    setErrorMessage(message);
+
+    setTimeout(() => {
+      setErrorMessage("");
+    }, 5000);
+  };
+
+  useEffect(() => {
+    if (title.length < 1) setIsDisabled(true);
+  }, [title]);
+
+  return {
+    // getters
+    body,
+    errorMessage,
+    isDisabled,
+    title,
+
+    // setters
+    onSetErrorMessage,
+    setBody,
+    setIsDisabled,
+    onChangeTitle,
+  };
+};
