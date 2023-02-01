e69829721dd66fee0ecfdabba4f1add4949809b7
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-31 23:57:50 -0300

    feat: :sparkles: add FormBottomSubmit component

---
+++ components/form/FormBottomSubmit.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  label: string;
+  disabled?: boolean;
+}
+
+export const FormBottomSubmit: React.FC<Props> = ({
+  label,
+  disabled = false,
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      type="submit"
+      className="w-full cursor-pointer rounded-[10px]  border border-background bg-background py-2 text-lg  font-semibold text-white outline-none duration-300 hover:bg-white hover:text-background disabled:cursor-not-allowed disabled:border-background/20 disabled:bg-background/20 disabled:text-white"
+    >
+      {label}
+    </button>
+  );
+};
