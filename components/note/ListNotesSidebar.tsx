8a11b03811ed582eb4e0d01492440f042ef7d8cb
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-03 21:33:36 -0300

    feat: :sparkles: add ListNotesSidebar component

---
+++ components/note/ListNotesSidebar.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+
+//* components *//
+import { NotePreview } from "./NotePreview";
+
+//* context *//
+import { NotesContext } from "../../context";
+
+export const ListNotesSidebar = () => {
+  const { notes } = useContext(NotesContext);
+
+  return (
+    <section className="hidden max-h-screen w-[350px] border-r border-orange/30 xl:flex xl:flex-col xl:overflow-y-scroll">
+      {notes.map((note) => (
+        <NotePreview key={note.id} note={note} />
+      ))}
+    </section>
+  );
+};
