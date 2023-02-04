c4e6730aea7e9298a79109f5a62cdc9f0f2e9b30
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-04 19:36:13 -0300

    feat: :sparkles: add new buttons and delete note functionality on NoteHeader

--- components/note/NoteHeader.tsx
+++ components/note/NoteHeader.tsx
@@ -1,15 +1,44 @@
 import { useContext } from "react";
+import { useRouter } from "next/router";
+import Swal from "sweetalert2";
 
 //* icons *//
-import { MdCheck, MdMoreHoriz } from "react-icons/md";
-import { IoContract, IoExpand } from "react-icons/io5";
+import { MdCheck, MdDeleteOutline, MdOutlineMode } from "react-icons/md";
+import { RiFullscreenFill, RiFullscreenExitFill } from "react-icons/ri";
 
 //* context *//
 import { NotesContext, UIContext } from "../../context";
 
 export const NoteHeader = () => {
   const { listNoteSidebar, toggleListNoteSidebar } = useContext(UIContext);
-  const { note, isEditing, setIsSaving } = useContext(NotesContext);
+  const { note, notes, isEditing, setIsSaving, setIsEditing, startDeleteNote } =
+    useContext(NotesContext);
+
+  const router = useRouter();
+
+  //! delete note
+  const onDeleteNote = async () => {
+    Swal.fire({
+      title: "¿Quieres borrar la nota?",
+      showCancelButton: true,
+      confirmButtonColor: "#a00",
+      confirmButtonText: "Borrar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const noteId = note!.id;
+
+        const result = await startDeleteNote(noteId!);
+        if (result) {
+          Swal.fire("Nota borrada con exito.", "", "success");
+          router.replace(
+            `/notes/${notes[0].id === note!.id ? notes[1].id : notes[0].id}`
+          );
+        } else {
+          Swal.fire("Error al borrar la nota.", "", "error");
+        }
+      }
+    });
+  };
 
   return (
     <>
@@ -25,35 +54,47 @@ export const NoteHeader = () => {
           </time>
         </div>
       </div>
-      <div className="sticky top-0 z-50 flex flex-col rounded-[26px] bg-white px-4 py-2">
+      <div className="flex flex-col rounded-[26px] bg-white px-4 py-2">
         <div className="flex w-full">
-          <div className="group flex items-center justify-center rounded-full bg-white hover:bg-background  ">
+          <div className="group hidden items-center justify-center rounded-full bg-white hover:bg-background xl:flex  ">
             <button
-              onClick={toggleListNoteSidebar}
+              onClick={() => toggleListNoteSidebar()}
               className="h-full w-full p-1 px-2"
             >
               {listNoteSidebar ? (
-                <IoContract className="text-xl text-background duration-300 group-hover:text-white" />
+                <RiFullscreenFill className="text-xl text-background duration-300 group-hover:text-white" />
               ) : (
-                <IoExpand className="text-xl text-background duration-300 group-hover:text-white" />
+                <RiFullscreenExitFill className="text-xl text-background duration-300 group-hover:text-white" />
               )}
             </button>
           </div>
+
           {isEditing ? (
             <div className="group ml-2 flex items-center justify-center rounded-full bg-white p-1 duration-300 hover:bg-greenLight">
               <button
-                onClick={() => {
-                  setIsSaving(true);
-                }}
+                onClick={() => setIsSaving(true)}
                 className="h-full w-full"
               >
                 <MdCheck className="text-2xl text-greenLight duration-300 group-hover:text-white" />
               </button>
             </div>
-          ) : null}
-          <div className="group ml-auto flex items-center justify-center rounded-full bg-white p-1 duration-300 hover:bg-background">
-            <button className="h-full w-full">
-              <MdMoreHoriz className="text-2xl text-background duration-300 group-hover:text-white" />
+          ) : (
+            <div className="group ml-2 flex items-center justify-center rounded-full bg-white p-1 duration-300 hover:bg-yellow">
+              <button
+                onClick={() => setIsEditing(true)}
+                className="h-full w-full"
+              >
+                <MdOutlineMode className="text-2xl text-yellow duration-300 group-hover:text-white" />
+              </button>
+            </div>
+          )}
+
+          <div className="group relative ml-auto flex items-center justify-center">
+            <button
+              onClick={onDeleteNote}
+              className="h-full w-full rounded-full bg-white p-1 duration-300 group-hover:bg-red-600"
+            >
+              <MdDeleteOutline className="text-2xl text-red-600 duration-300 hover:text-white group-hover:text-white" />
             </button>
           </div>
         </div>
